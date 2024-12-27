7720d731f60086ee99658222699c305c152ab097
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-27 17:05:46 -0300

    Validación con express validator

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,19 @@
 import {Request, Response} from 'express'
+import {validationResult} from 'express-validator'
 import slug from 'slug'
+
 import User from "../models/User"
-import {hash} from 'bcrypt'
 import {hashPassword} from '../utils/auth'
 
+
 export const createAccount = async (req: Request, res: Response) => {
+    // Manejar errores
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        res.status(400).json({errors: errors.array()})
+        return 
+    }
+    
     const {email, password} = req.body
     // Comprobación email
     const userExists = await User.findOne({email})
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,14 @@
 import {Router} from 'express'
+import {body} from 'express-validator'
 import {createAccount} from './handlers'
 const router = Router()
 
 // Routing
-router.post('/auth/register',createAccount) 
-
-router.get('/bromita', (req, res) => {
-    res.send('Has sido JAJAJAckeado :)')
-})
+router.post('/auth/register',
+    body("handle")
+        .notEmpty()
+        .withMessage('Campo vacío'),
+    createAccount
+)
 
 export default router
