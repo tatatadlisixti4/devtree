420629193e6b3600c15b35689e3b9885bbd279ed
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-27 01:36:17 -0300

    Configurando mensajes con colors

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose'
+import colors from 'colors'
 
 export const connectDB = async () => {
     try {
         const {connection} = await mongoose.connect(process.env.MONGO_URI)
         const url = `${connection.host}: ${connection.port}`
-        console.log(`MongoDB conectado en ${url}`)
+        console.log(colors.cyan.bold(`MongoDB conectado en ${url}`))
         
     } catch (error) {
-        console.log(error.message)
+        console.log(colors.bgRed.white.bold(error.message))
         process.exit(1)
-        
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import server from './server'
+import colors from 'colors'
 
 // Puerto y servidor
 const port = process.env.PORT || 4000
 server.listen(port, () => {
-    console.log('Servidor Funcionando en el puerto: ', port)
+    console.log(colors.bgMagenta.blue.white('Servidor Funcionando en el puerto: '), port)
 })
