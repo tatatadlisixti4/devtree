1cd84d69370aff1f5e628701e9c43a14cd197894
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-27 01:28:06 -0300

    Cambiando parametros conexión bd con variables de entorno vía dotenv

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,9 @@ import mongoose from 'mongoose'
 
 export const connectDB = async () => {
     try {
-        const url = 'mongodb+srv://root:s55OLKsxbuedKC6H@cluster0.rjwag.mongodb.net/linktree_node_typescript'
-        const {connection} = await mongoose.connect(url)
-        const info = `${connection.host}: ${connection.port}`
-        console.log(`MongoDB conectado en ${info}`)
+        const {connection} = await mongoose.connect(process.env.MONGO_URI)
+        const url = `${connection.host}: ${connection.port}`
+        console.log(`MongoDB conectado en ${url}`)
         
     } catch (error) {
         console.log(error.message)
