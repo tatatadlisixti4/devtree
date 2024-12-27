a2d3c04e78d99947983f5407a33b4f3173292871
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-27 13:42:15 -0300

    Hashear passwords de usuarios

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,16 +1,18 @@
 import {Request, Response} from 'express'
 import User from "../models/User"
+import { hash } from 'bcrypt'
+import { hashPassword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
-    const {email} = req.body
+    const {email, password} = req.body
     const userExists = await User.findOne({email})
     if(userExists) {
         const error = new Error('El usuario ya está registrado')
         res.status(409).json({error: error.message})
         return
     }
-    
     const user = new User(req.body)
+    user.password = await hashPassword(password)
     await user.save()
     res.status(201).send('Registro creado correctamente :)')
 }
