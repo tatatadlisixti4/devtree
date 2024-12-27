68a58c07bba792e7ef6e4f2d00ab9bf686c45c70
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-27 00:28:58 -0300

    Inicio routing aut

--- src/router.ts
+++ src/router.ts
@@ -3,9 +3,11 @@ import {Router} from 'express'
 const router = Router()
 
 // Routing
-router.get('/', (req, res) => {
-    res.send('Hola Mundo en Express | TypeScript')
+router.get('/auth/register', (req, res) => {
+    console.log('Desde Register')
+    
 })
+
 router.get('/bromita', (req, res) => {
     res.send('Has sido JAJAJAckeado :)')
 })
