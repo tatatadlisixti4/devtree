4a9a5dbba68ecbb33d35a228c38bcd27044db859
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-27 12:36:11 -0300

    Registro nuestro primer usuario en la bd

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,12 @@
 import {Router} from 'express'
-
+import User from './models/User'
 const router = Router()
 
 // Routing
-router.post('/auth/register', (req, res) => {
-    console.log(req.body)
-})
+router.post('/auth/register', async (req, res) => {
+    const user = new User(req.body)
+    await user.save()
+}) 
 
 router.get('/bromita', (req, res) => {
     res.send('Has sido JAJAJAckeado :)')
