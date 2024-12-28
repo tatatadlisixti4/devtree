c96019eef6bf82caa306eb034b5fe5b0889c709e
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-28 00:19:35 -0300

    Navegando entre páginas con React Router

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,6 +1,14 @@
+import {Link} from "react-router-dom"
 
 export default function LoginView() {
     return (
-        <div className="text-6xl">LoginView</div>
+        <>
+            <div className="text-3xl">LoginView</div>
+            <nav>
+                <Link to="/auth/register">
+                    ¿No tienes cuenta? Crea una aquí
+                </Link>
+            </nav>
+        </>
     )
 }
