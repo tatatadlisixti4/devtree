109341c7186c6d18c61b143e28ca61fdc2e012ec
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-28 12:50:03 -0300

    Creando el layout principal para autenticación

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,14 +1,16 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import LoginView from './views/LoginView'
 import RegisterView from './views/RegisterView'
+import AuthLayout from './layouts/AuthLayout'
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/auth/login' element={<LoginView />}></Route>
-                <Route path='/auth/register' element={<RegisterView />}></Route>
-                
+                <Route element={<AuthLayout />}>
+                    <Route path='/auth/login' element={<LoginView />}></Route>
+                    <Route path='/auth/register' element={<RegisterView />}></Route>
+                </Route>
             </Routes>
         
         </BrowserRouter>
--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,7 +3,6 @@ import {Link} from "react-router-dom"
 export default function LoginView() {
     return (
         <>
-            <div className="text-3xl">LoginView</div>
             <nav>
                 <Link to="/auth/register">
                     ¿No tienes cuenta? Crea una aquí
