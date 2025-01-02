9f75b6ca841ae2b778e27aed816a1295189ccc70
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-02 09:17:55 -0300

    Añadiendo el formulario useForm

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -9,3 +9,7 @@ export type RegisterForm = Pick<User, 'handle' | 'name' | 'email'> & {
     password: string
     password_confirmation: string
 }
+
+export type LoginForm = Pick<User, 'email'> & {
+    password: string
+}
