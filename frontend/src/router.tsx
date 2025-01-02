e4f2cfb7a76f8815c557ce7980d95058aa355013
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-02 16:22:39 -0300

    Creando el layout de administración

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,14 +2,22 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import LoginView from './views/LoginView'
 import RegisterView from './views/RegisterView'
 import AuthLayout from './layouts/AuthLayout'
+import AppLayout from './layouts/AppLayout'
+import LinkTreeView from './views/LinkTreeView'
+import ProfileView from './views/ProfileView'
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<AuthLayout />}>
-                    <Route path='/auth/login' element={<LoginView />}></Route>
-                    <Route path='/auth/register' element={<RegisterView />}></Route>
+                    <Route path='/auth/login' element={<LoginView />} />
+                    <Route path='/auth/register' element={<RegisterView />} />
+                </Route>
+                
+                <Route path='admin' element={<AppLayout />}>
+                    <Route index={true} element={<LinkTreeView />} />
+                    <Route path='profile' element={<ProfileView />} />
                 </Route>
             </Routes>
         
