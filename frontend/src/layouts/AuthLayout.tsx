1173da440c370898e4d59c75b2e8227c79e8ea86
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-01 22:55:43 -0300

    Mostrando los mensajes del backend en un toast de sonner

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import {Outlet} from 'react-router-dom'
+import {Toaster} from 'sonner'
 
 export default function AuthLayout() {
     return (
@@ -6,12 +7,12 @@ export default function AuthLayout() {
             <div className="bg-slate-800 min-h-screen">
                 <div className="max-w-lg mx-auto pt-10 px-5">
                     <img src="/logo.svg" alt="Logo DevTree"></img> 
-
                     <div className="py-10">
                         <Outlet />
                     </div>
                 </div>
             </div>
+            <Toaster position="top-right" />
         </>   
     )
 }
--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,6 +1,7 @@
 import {Link} from "react-router-dom"
 import {useForm} from "react-hook-form"
 import axios, {isAxiosError} from 'axios'
+import {toast} from 'sonner'
 
 import {RegisterForm} from "../types"
 import ErrorMessage from "../components/ErrorMessage"
@@ -16,16 +17,15 @@ export default function RegisterView() {
 
     const {register, watch, reset, handleSubmit, formState: {errors}} = useForm({defaultValues: initialValues})
     const password = watch('password')
-    console.log()
     
     const handleRegister = async (formData: RegisterForm) => {
         try {
             const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, formData)
-            console.log(data)
+            toast.success(data.response)
             reset()
         } catch (error) {
             if(isAxiosError(error) && error.response){
-                console.log(error.response.data.error) 
+                toast.error(error.response.data.error) 
             }
         }
     }
