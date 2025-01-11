a3846f6c36b865def54d85f720e6300a9f731254
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-11 18:07:02 -0300

    Subiendo imágenes con Cloudinary

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -2,6 +2,7 @@ import type {Request, Response} from 'express'
 import {validationResult} from 'express-validator'
 import slug from 'slug'
 import formidable from 'formidable'
+import {v4 as uuid} from 'uuid'
 
 import User from "../models/User"
 import {hashPassword, checkPassword} from '../utils/auth'
@@ -101,11 +102,18 @@ export const updateProfile = async(req: Request, res: Response) => {
 
 export const uploadImage = async(req: Request, res: Response) => {
     const form = formidable({multiples: false})
-    form.parse(req, (error, fields, files) => {
-        console.log(files.file[0].filepath)
-    })
     try {
-        console.log('Desde uploadImage')
+        form.parse(req, (error, fields, files) => {
+            cloudinary.uploader.upload(files.file[0].filepath, {public_id: uuid()}, async function(error, result) {
+                if(error) {
+                    const error = new Error('Hubo un error al subir la imagen')
+                    res.status(500).json({error: error.message})
+                }
+                if(result) {
+                    console.log(result.secure_url)
+                }
+            })
+        })
         
     } catch (e) {
         const error = new Error('Hubo un error')
