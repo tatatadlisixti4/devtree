e9f5f7077049f8169d988ee8c0c118528210f74b
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-13 21:41:56 -0300

    Añadiendo un switch para habilitar y deshabilitar redes sociales

--- frontend/src/components/DevTreeInput.tsx
+++ frontend/src/components/DevTreeInput.tsx
@@ -1,4 +1,6 @@
+import {Switch} from '@headlessui/react'
 import {DevTreeLink} from "../types"
+import {classNames} from '../utils'
 
 type DevTreeItemProps = {
     item: DevTreeLink
@@ -15,6 +17,23 @@ export default function DevTreeInput({item}: DevTreeItemProps) {
                 type="text" 
                 className="flex-1 border border-gray-100 rounded-lg"
             />
+
+            <Switch
+                checked={item.enabled}
+                onChange={() => {}}
+                className={classNames(
+                    item.enabled ? 'bg-blue-500' : 'bg-gray-200',
+                    'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+                )}
+            >
+                <span
+                    aria-hidden="true"
+                    className={classNames(
+                        item.enabled ? 'translate-x-5' : 'translate-x-0',
+                        'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
+                    )}
+                />
+            </Switch>
         </div>
     )
 }
