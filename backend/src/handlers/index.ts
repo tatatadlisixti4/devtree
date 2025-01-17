f1cd7f0efcd39a9ce9a893720685825c45c80ab5
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-17 13:46:55 -0300

    Consultando la base de datos y mostrando la información en base al handle

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -128,7 +128,14 @@ export const uploadImage = async(req: Request, res: Response) => {
 export const getUserByHandle = async (req: Request, res: Response) => {
     try {
         const {handle} = req.params
-        console.log(handle)
+        const user = await User.findOne({handle}).select('-_id -__v -email -password')
+        
+        if(!user) {
+            const error = new Error('El usuario no existe')
+            res.status(404).json({error: error.message})
+            return
+        } 
+        res.json(user)
         
     } catch (e) {
         const error = new Error('Hubo un error')
