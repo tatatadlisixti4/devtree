b0808f480ccdf0d22601baab34970ec61c8ca5d9
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-19 17:37:45 -0300

    Creando la página 404

--- frontend/src/views/HandleView.tsx
+++ frontend/src/views/HandleView.tsx
@@ -1,5 +1,5 @@
 import {useQuery} from '@tanstack/react-query'
-import {useParams} from 'react-router-dom'
+import {Navigate, useParams} from 'react-router-dom'
 import {getUserByHandle} from '../api/DevTreeAPI'
 export default function HandleView() {
     const params = useParams()
@@ -11,9 +11,9 @@ export default function HandleView() {
         retry: 1,
         refetchOnWindowFocus: false
     })
-    console.log(isLoading)
-    console.log(error)
-    console.log(data)
+    if(isLoading) return <p className='text-center text-white'>Cargando...</p>
+    if(error) return <Navigate to={'/404'} /> 
+
     return (
         <div>HandleView</div>
     )
