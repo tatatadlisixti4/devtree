e8623859f50675c8c19b7c67e45db1d659e0a852
Thadli Guerra <tg.ibarra64@gmail.com>
2025-01-19 17:52:36 -0300

    Mostrando el handle, imagen y descripción

--- frontend/src/components/HandleData.tsx
+++ frontend/src/components/HandleData.tsx
@@ -5,6 +5,10 @@ type HandleDataProps = {
 }
 export default function HandleData({data} : HandleDataProps) {
     return (
-        <div>{data.handle}</div>
+        <div className="space-y-6 text-white">
+            <p className="text-5xl text-center font-black">{data.handle}</p>
+            {data.image && <img src={data.image } className="max-w-[250px] mx-auto"/>}
+            <p className="text-lg text-center font-bold" >{data.description}</p>
+        </div>
     )
 }
